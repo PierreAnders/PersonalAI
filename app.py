93e0549a48d37a683a1655d01a2a1f95dd5271c5
PierreAnders <pierre.untas@gmail.com>
2023-07-04 15:12:37 +0200

    add a swagger

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import sys
-
+from flasgger import Swagger
 import openai
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
@@ -17,7 +17,9 @@ import constants
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
+
 app = Flask(__name__)
+swagger = Swagger(app)
 CORS(app)
 
 # Enable to save to disk & reuse the model (for repeated queries on the same data)
@@ -35,7 +37,7 @@ else:
     index = VectorstoreIndexCreator().from_loaders([loader])
 
 chain = ConversationalRetrievalChain.from_llm(
-    llm=ChatOpenAI(model="gpt-3.5-turbo"),
+    llm=ChatOpenAI(model="gpt-3.5-turbo-16k"),
     retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
 )
 
@@ -44,23 +46,43 @@ chat_histories = {}
 
 @app.route('/chat', methods=['POST'])
 def chat():
+    """
+    Cette API lance une conversation avec le bot
+    ---
+    tags:
+      - Chat API
+    parameters:
+    - in: body
+      name: body
+      schema:
+        type: object
+        properties:
+          session_id:
+            type: string
+            description: L'ID de la session
+          query:
+            type: string
+            description: La question posée par l'utilisateur
+        required:
+          - session_id
+          - query
+    responses:
+      200:
+        description: Réponse du bot
+    """
     data = request.get_json()
     session_id = data.get("session_id")
     query = data.get("query")
 
-    # Obtenez l'historique de la conversation pour cette session
     chat_history = chat_histories.get(session_id, [])
 
-    # Obtenez la réponse du bot en utilisant la chaîne de conversation
     result = chain({"question": query, "chat_history": chat_history})
 
-    # Ajoutez la question et la réponse à l'historique de la conversation
     chat_history.append((query, result['answer']))
     chat_histories[session_id] = chat_history
 
-    # Pour cet exemple, nous supposons que 'result' est un dictionnaire qui contient une clé 'answer' avec la réponse du bot.
-    # Modifiez ceci en fonction de la structure réelle de votre 'result'.
     return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
