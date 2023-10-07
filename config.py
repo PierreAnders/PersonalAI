5f6b1df6de8ce79585708056f1ae3c2cb559c72a
PierreAnders <pierre.untas@gmail.com>
2023-10-07 22:50:19 +0200

    add secret key and url db in dot env

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
-# import os
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = "50c34e5139cb598bf297a67910047d29a422b1bd828557ec"
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:3568@localhost/aria'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+
+
+
+# class Config:
+#     SECRET_KEY = "50c34e5139cb598bf297a67910047d29a422b1bd828557ec"
+#     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:3568@localhost/aria'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # basedir = os.path.abspath(os.path.dirname(__file__))
 # class Config:
