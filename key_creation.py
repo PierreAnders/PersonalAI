1c8a1b7e302c531f77aa79241d3590b74917b29b
PierreAnders <pierre.untas@gmail.com>
2023-10-29 14:43:24 +0100

    develop mode ok

--- key_creation.py
+++ key_creation.py
@@ -8,6 +8,3 @@ secret_key_hex = secret_key.hex()
 
 print("Votre clé secrète Flask générée aléatoirement :")
 print(secret_key_hex)
-
-# Votre clé secrète Flask générée aléatoirement :
-# 50c34e5139cb598bf297a67910047d29a422b1bd828557ec
