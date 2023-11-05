cc98a2e90bd7ebac5c973013fb5db95f13e4d111
PierreAnders <pierre.untas@gmail.com>
2023-11-05 11:13:37 +0100

    refacto incomes with a service

--- app/incomes/routes.py
+++ app/incomes/routes.py
@@ -1,67 +1,31 @@
-from app.incomes import bp 
-from app.extensions import db
-from app.incomes.model import Income 
+from app.incomes import bp
+from app.incomes.model import Income
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from flask import request, jsonify 
-from sqlalchemy.exc import IntegrityError
-
+from flask import request, jsonify
+from app.incomes.service import add_income, delete_income, get_all_incomes
 
 @bp.route('/incomes', methods=['POST'])
 @jwt_required()
-def add_income():
+def add_income_route():
     data = request.get_json()
     title = data.get("title")
     description = data.get("description")
     price = data.get("price")
-
     user_id = get_jwt_identity()
-
-    income = Income(title=title, description=description, price=price, user_id=user_id)
-
-    try:
-        db.session.add(income)
-        db.session.commit()
-        return jsonify({"message": "Recette ajoutée avec succès"}), 201
-    except IntegrityError as e:
-        db.session.rollback()
-        bp.app.logger.error("Erreur de base de données lors de l'ajout de la recette : " + str(e))
-        return jsonify({"message": "Erreur de base de données : " + str(e)}), 500
-
+    
+    result, status_code = add_income(title, description, price, user_id)
+    return jsonify(result), status_code
 
 @bp.route('/incomes/<int:income_id>', methods=['DELETE'])
 @jwt_required()
-def delete_income(income_id):
+def delete_income_route(income_id):
     user_id = get_jwt_identity()
+    result, status_code = delete_income(income_id, user_id)
+    return jsonify(result), status_code
 
-    income = Income.query.filter_by(id=income_id, user_id=user_id).first()
-
-    if not income:
-        return jsonify({"message": "Recette non trouvée ou non autorisée"}), 404
-
-    try:
-        db.session.delete(income)
-        db.session.commit()
-        return jsonify({"message": "Recette supprimée avec succès"}), 200
-    except IntegrityError as e:
-        db.session.rollback()
-        return jsonify({"message": "Erreur de base de données : " + str(e)}), 500
-    
-    
 @bp.route('/incomes', methods=['GET'])
 @jwt_required()
-def get_all_incomes():
+def get_all_incomes_route():
     user_id = get_jwt_identity()
-
-    incomes = Income.query.filter_by(user_id=user_id).all()
-
-    incomes_data = []
-
-    for income in incomes:
-        incomes_data.append({
-            "id": income.id,
-            "title": income.title,
-            "description": income.description,
-            "price": str(income.price) 
-        })
-
-    return jsonify(incomes_data), 200
+    incomes_data, status_code = get_all_incomes(user_id)
+    return jsonify(incomes_data), status_code
---
+++ app/incomes/service.py
@@ -0,0 +1,38 @@
+from app.extensions import db
+from app.incomes.model import Income
+from sqlalchemy.exc import IntegrityError
+
+def add_income(title, description, price, user_id):
+    income = Income(title=title, description=description, price=price, user_id=user_id)
+    try:
+        db.session.add(income)
+        db.session.commit()
+        return {"message": "Recette ajoutée avec succès"}, 201
+    except IntegrityError as e:
+        db.session.rollback()
+        error_message = "Erreur de base de données lors de l'ajout de la recette : " + str(e)
+        return {"message": error_message}, 500
+
+def delete_income(income_id, user_id):
+    income = Income.query.filter_by(id=income_id, user_id=user_id).first()
+    if not income:
+        return {"message": "Recette non trouvée ou non autorisée"}, 404
+    try:
+        db.session.delete(income)
+        db.session.commit()
+        return {"message": "Recette supprimée avec succès"}, 200
+    except IntegrityError as e:
+        db.session.rollback()
+        return {"message": "Erreur de base de données : " + str(e)}, 500
+
+def get_all_incomes(user_id):
+    incomes = Income.query.filter_by(user_id=user_id).all()
+    incomes_data = []
+    for income in incomes:
+        incomes_data.append({
+            "id": income.id,
+            "title": income.title,
+            "description": income.description,
+            "price": str(income.price)
+        })
+    return incomes_data, 200
