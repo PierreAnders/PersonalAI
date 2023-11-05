4adeaf1dd4ed0b4ec1258c0d34998d27df5c0185
PierreAnders <pierre.untas@gmail.com>
2023-11-05 11:08:09 +0100

    refacto expenses with a service

--- app/expenses/routes.py
+++ app/expenses/routes.py
@@ -1,67 +1,31 @@
-from app.expenses import bp 
-from app.extensions import db
-from app.expenses.model import Expense 
+from app.expenses import bp
+from app.expenses.model import Expense
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from flask import request, jsonify 
-from sqlalchemy.exc import IntegrityError
-
+from flask import request, jsonify
+from app.expenses.service import add_expense, delete_expense, get_all_expenses
 
 @bp.route('/expenses', methods=['POST'])
 @jwt_required()
-def add_expense():
+def add_expense_route():
     data = request.get_json()
     title = data.get("title")
     description = data.get("description")
     price = data.get("price")
-
     user_id = get_jwt_identity()
-
-    expense = Expense(title=title, description=description, price=price, user_id=user_id)
-
-    try:
-        db.session.add(expense)
-        db.session.commit()
-        return jsonify({"message": "Dépense ajoutée avec succès"}), 201
-    except IntegrityError as e:
-        db.session.rollback()
-        bp.app.logger.error("Erreur de base de données lors de l'ajout de la dépense : " + str(e))
-        return jsonify({"message": "Erreur de base de données : " + str(e)}), 500
-
+    
+    result, status_code = add_expense(title, description, price, user_id)
+    return jsonify(result), status_code
 
 @bp.route('/expenses/<int:expense_id>', methods=['DELETE'])
 @jwt_required()
-def delete_expense(expense_id):
+def delete_expense_route(expense_id):
     user_id = get_jwt_identity()
+    result, status_code = delete_expense(expense_id, user_id)
+    return jsonify(result), status_code
 
-    expense = Expense.query.filter_by(id=expense_id, user_id=user_id).first()
-
-    if not expense:
-        return jsonify({"message": "Dépense non trouvée ou non autorisée"}), 404
-
-    try:
-        db.session.delete(expense)
-        db.session.commit()
-        return jsonify({"message": "Dépense supprimée avec succès"}), 200
-    except IntegrityError as e:
-        db.session.rollback()
-        return jsonify({"message": "Erreur de base de données : " + str(e)}), 500
-    
-    
 @bp.route('/expenses', methods=['GET'])
 @jwt_required()
-def get_all_expenses():
+def get_all_expenses_route():
     user_id = get_jwt_identity()
-
-    expenses = Expense.query.filter_by(user_id=user_id).all()
-
-    expenses_data = []
-
-    for expense in expenses:
-        expenses_data.append({
-            "id": expense.id,
-            "title": expense.title,
-            "description": expense.description,
-            "price": str(expense.price) 
-        })
-
-    return jsonify(expenses_data), 200
+    expenses_data, status_code = get_all_expenses(user_id)
+    return jsonify(expenses_data), status_code
---
+++ app/expenses/service.py
@@ -0,0 +1,38 @@
+from app.extensions import db
+from app.expenses.model import Expense
+from sqlalchemy.exc import IntegrityError
+
+def add_expense(title, description, price, user_id):
+    expense = Expense(title=title, description=description, price=price, user_id=user_id)
+    try:
+        db.session.add(expense)
+        db.session.commit()
+        return {"message": "Dépense ajoutée avec succès"}, 201
+    except IntegrityError as e:
+        db.session.rollback()
+        error_message = "Erreur de base de données lors de l'ajout de la dépense : " + str(e)
+        return {"message": error_message}, 500
+
+def delete_expense(expense_id, user_id):
+    expense = Expense.query.filter_by(id=expense_id, user_id=user_id).first()
+    if not expense:
+        return {"message": "Dépense non trouvée ou non autorisée"}, 404
+    try:
+        db.session.delete(expense)
+        db.session.commit()
+        return {"message": "Dépense supprimée avec succès"}, 200
+    except IntegrityError as e:
+        db.session.rollback()
+        return {"message": "Erreur de base de données : " + str(e)}, 500
+
+def get_all_expenses(user_id):
+    expenses = Expense.query.filter_by(user_id=user_id).all()
+    expenses_data = []
+    for expense in expenses:
+        expenses_data.append({
+            "id": expense.id,
+            "title": expense.title,
+            "description": expense.description,
+            "price": str(expense.price)
+        })
+    return expenses_data, 200
