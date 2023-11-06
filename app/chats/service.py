814121ed3fe7df2400e06e4b9a41c3eaa07914cb
PierreAnders <pierre.untas@gmail.com>
2023-11-06 17:32:31 +0100

    add prints in chats method

--- app/chats/service.py
+++ app/chats/service.py
@@ -17,6 +17,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 chat_histories = {}
 
+# !!! CODE BON !!!
 
 def chat_with_data_service(model, data):
     PERSIST = False
@@ -73,3 +74,67 @@ def generate_response(chain, query, chat_history):
 def update_chat_history(session_id, chat_history, query, answer):
     chat_history.append((query, answer))
     chat_histories[session_id] = chat_history
+
+
+# def chat_with_data_service(model, data):
+#     PERSIST = False
+
+#     index = get_or_create_index(PERSIST)
+#     print('index :', index)
+
+#     chain = create_conversational_chain(model, index)
+#     print('chain :', chain)
+
+#     session_id = data.get("session_id")
+#     print('session_id :', session_id)
+#     query = data.get("query")
+#     print('query :', query)
+
+#     chat_history = get_chat_history(session_id, chat_histories)
+#     print('chat_history :', chat_history)
+#     result = generate_response(chain, query, chat_history)
+#     print('result :', result)
+
+#     update_chat_history(session_id, chat_history, query, result['answer'])
+#     print('chat_history après update :', chat_history)
+
+#     return result
+
+
+# def get_or_create_index(persist):
+#     if persist and os.path.exists("persist"):
+#         vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+#         index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+#     else:
+#         user_id = get_jwt_identity()
+#         user_data_folder = f'data/{user_id}/'
+#         subdirs = [folder.path for folder in os.scandir(user_data_folder) if folder.is_dir()]
+#         loaders = [DirectoryLoader(subdir) for subdir in subdirs]
+#         if persist:
+#             index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders(loaders)
+#         else:
+#             index = VectorstoreIndexCreator().from_loaders(loaders)
+#     return index
+
+
+# def create_conversational_chain(model, index):
+#     chain = ConversationalRetrievalChain.from_llm(
+#         llm=ChatOpenAI(model=model),
+#         retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+#     )
+#     return chain
+
+
+# def get_chat_history(session_id, chat_histories):
+#     chat_history = chat_histories.get(session_id, [])
+#     return chat_history
+
+
+# def generate_response(chain, query, chat_history):
+#     result = chain({"question": query, "chat_history": chat_history})
+#     return result
+
+
+# def update_chat_history(session_id, chat_history, query, answer):
+#     chat_history.append((query, answer))
+#     chat_histories[session_id] = chat_history
--- app/code/service.py
+++ app/code/service.py
@@ -1,5 +1,5 @@
 import os
-# from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import get_jwt_identity
 import openai
 from dotenv import load_dotenv
 
@@ -11,55 +11,55 @@ chat_histories = {}
 
 # !!! CODE BON !!!
 
-# def chat_service(model, data):
-
-#     session_id = data.get("session_id")
-#     query = data.get("query")
-
-#     chat_history = chat_histories.get(session_id, [])
-#     chat_history.append((query, ""))
-
-#     messages = [{"role": "user", "content": msg} for msg, _ in chat_history]
-
-#     response = openai.ChatCompletion.create(
-#         model=model,
-#         messages=messages)
-
-#     assistant_reply = response["choices"][0]["message"]["content"]
-
-#     chat_history[-1] = (query, assistant_reply)
-#     chat_histories[session_id] = chat_history
-
-#     return {"answer": assistant_reply}
-
-
 def chat_service(model, data):
-    session_id = data.get("session_id")
-    print('session_id :', session_id)
 
+    session_id = data.get("session_id")
     query = data.get("query")
-    print('query :', query) 
 
     chat_history = chat_histories.get(session_id, [])
-    print('chat_history avant append :', chat_history)
-
     chat_history.append((query, ""))
-    print('chat_history après append :', chat_history)
 
     messages = [{"role": "user", "content": msg} for msg, _ in chat_history]
-    print('messages :', messages)
 
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages)
-    print('response :', response)
 
     assistant_reply = response["choices"][0]["message"]["content"]
-    print('assistant_reply :', assistant_reply)
 
     chat_history[-1] = (query, assistant_reply)
-    print('chat_history après update :', chat_history)
-    chat_histories[session_id] = chat_history   
-    print('chat_histories après update :', chat_histories)
-    
+    chat_histories[session_id] = chat_history
+
     return {"answer": assistant_reply}
+
+
+# def chat_service(model, data):
+#     user_id = get_jwt_identity()
+#     session_id = user_id
+
+#     query = data.get("query")
+#     print('query :', query) 
+
+#     chat_history = chat_histories.get(session_id, [])
+#     print('chat_history avant append :', chat_history)
+
+#     chat_history.append((query, ""))
+#     print('chat_history après append :', chat_history)
+
+#     messages = [{"role": "user", "content": msg} for msg, _ in chat_history]
+#     print('messages :', messages)
+
+#     response = openai.ChatCompletion.create(
+#         model=model,
+#         messages=messages)
+#     print('response :', response)
+
+#     assistant_reply = response["choices"][0]["message"]["content"]
+#     print('assistant_reply :', assistant_reply)
+
+#     chat_history[-1] = (query, assistant_reply)
+#     print('chat_history après update :', chat_history)
+#     chat_histories[session_id] = chat_history   
+#     print('chat_histories après update :', chat_histories)
+    
+#     return {"answer": assistant_reply}
