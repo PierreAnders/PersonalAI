60a07c7528d8816b8a84c2851b444c7e47913aa9
PierreAnders <pierre.untas@gmail.com>
2023-11-14 23:09:49 +0100

    add encrypt and decrypt features

--- utils/cryptography.py
+++ utils/cryptography.py
@@ -1,57 +1,33 @@
 from cryptography.fernet import Fernet
 import binascii
+import os
+from dotenv import load_dotenv
 
-key = b'5r41A3eWfP6AyRPG0xcozjhCyxR298T8h4m0Q2_xcNM='
-cipher_suite = Fernet(key)
+# Chargement des variables d'environnement depuis le fichier .env
+load_dotenv()
 
+# Récupération de la clé Fernet à partir des variables d'environnement
+FERNET_KEY = os.getenv('FERNET_KEY')
+
+# Encodage de la clé en tant que bytes
+cipher_suite = Fernet(FERNET_KEY.encode())
+
+# Fonction pour chiffrer les données
 def encrypt_data(data):
+    # Convertir la chaîne de données en bytes, puis chiffrer avec Fernet
     encrypted_data = cipher_suite.encrypt(data.encode())
-    print(f"Encrypted data: {encrypted_data}")
     return encrypted_data
 
+# Fonction pour déchiffrer les données
 def decrypt_data(hex_data):
     hex_data_no_prefix = hex_data[2:]  # Exclure le préfixe "0x"
+    
     try:
+        # Convertir le texte hexadécimal en données binaires
         binary_data = bytes.fromhex(hex_data_no_prefix)
+        # Déchiffrer les données binaires avec Fernet, puis décoder en UTF-8
         decrypted_data = cipher_suite.decrypt(binary_data).decode()
-        print(f"Decrypted data: {decrypted_data}")
         return decrypted_data
     except binascii.Error as e:
+        # Gérer les erreurs lors du décodage du texte hexadécimal
         print(f"Error decoding hex data: {str(e)}")
-
-
-# # Importer les modules nécessaires
-# from cryptography.fernet import Fernet
-# import binascii
-
-# # Clé de chiffrement Fernet préalablement générée
-# key = b'5r41A3eWfP6AyRPG0xcozjhCyxR298T8h4m0Q2_xcNM='
-
-# # Créer une instance de la classe Fernet avec la clé
-# cipher_suite = Fernet(key)
-
-# # Fonction pour chiffrer les données
-# def encrypt_data(data):
-#     # Convertir la chaîne de données en bytes, puis chiffrer avec Fernet
-#     encrypted_data = cipher_suite.encrypt(data.encode())
-#     # Afficher les données chiffrées
-#     print(f"Encrypted data: {encrypted_data}")
-#     # Renvoyer les données chiffrées
-#     return encrypted_data
-
-# # Fonction pour déchiffrer les données
-# def decrypt_data(hex_data):
-#     # Exclure le préfixe "0x" du texte hexadécimal
-#     hex_data_no_prefix = hex_data[2:]
-#     try:
-#         # Convertir le texte hexadécimal en données binaires
-#         binary_data = bytes.fromhex(hex_data_no_prefix)
-#         # Déchiffrer les données binaires avec Fernet, puis décoder en UTF-8
-#         decrypted_data = cipher_suite.decrypt(binary_data).decode()
-#         # Afficher les données déchiffrées
-#         print(f"Decrypted data: {decrypted_data}")
-#         # Renvoyer les données déchiffrées
-#         return decrypted_data
-#     except binascii.Error as e:
-#         # Gérer les erreurs lors du décodage du texte hexadécimal
-#         print(f"Error decoding hex data: {str(e)}")
